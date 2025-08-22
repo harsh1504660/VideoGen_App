25883ad9662ab5838508ffb89cf3930d1482920e
Harsh Joshi <77767961+harsh1504660@users.noreply.github.com>
2025-08-22 18:48:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # You can restrict this to your frontend URL
+    allow_origins=["*"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
